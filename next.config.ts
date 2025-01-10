9d9589ef987a04a76de7de25a81e0917f9071a3a
abbasslp <aspersiangolf@gmail.com>
2025-01-10 08:35:55 -0800

    رفع مشکل تصاویر و تغییر تنظیمات Netlify

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  devIndicators: {
-    buildActivity: true,
-  },
 };
+
+module.exports = nextConfig;
